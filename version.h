5f96e1c965b4188c20895ccddf78c751ec21cad2
Ixam97 <maxigoldschmidt@gmail.com>
2022-03-03 18:32:26 +0100

    [2022-03-03.2]

--- version.h
+++ version.h
@@ -1,9 +1,9 @@
 #pragma once
-#define VERSION_MAJOR 1
+#define VERSION_MAJOR 0
 #define VERSION_MINOR 0
 #define VERSION_PATCH 0
 #define VERSION_BUILD 1
-#define COMMIT_CODE "[2022-03-03.1]"
+#define COMMIT_CODE "[2022-03-03.2]"
 
 #define stringify(a) stringify_(a)
 #define stringify_(a) #a
